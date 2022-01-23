fe64ff688846906a427e2d66730a750052b9ad5e
Caglayn <caglayankaya@hotmail.com.tr>
2022-01-23 18:30:32 +0300

    Gateway working

---
+++ MobileAppUsers/src/test/java/com/caglayan/mobileappusers/MobileAppUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caglayan.mobileappusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
